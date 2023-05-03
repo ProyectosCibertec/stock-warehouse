7b0b7a963d406a5638238ca1c0ad7078b8fafb0b
JorgeChavezG <jorge10009@hotmail.com>
2023-05-02 23:04:00 -0500

    jorge
    Implementación de entidades y paquetes

---
+++ src/main/java/edu/cibertec/stockwarehouse/cabecerainventario/domain/model/CabeceraInventario.java
@@ -0,0 +1,53 @@
+package edu.cibertec.stockwarehouse.cabecerainventario.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "tb_cabecerainventario")
+public class CabeceraInventario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_cab_inventario")
+    private int id_cab_inventario;
+
+    @Column(name = "tipo_inventario")
+    private int tipo_inventario;
+
+    @Column(name = "fecha_cab_inventario", nullable = false)
+    @Temporal(value = TemporalType.DATE)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date fecha_cab_inventario;
+
+    @Column(name = "id_empleado")
+    private int empleado;
+
+    @Column(name = "estado")
+    private int estado;
+
+
+    //método para asignar nombre a los estados
+    public String NombreEstado() {
+        String nombreEstado = null;
+        switch (estado) {
+            case 0:
+                nombreEstado = "Activo";
+                break;
+            case 1:
+                nombreEstado = "Anulado";
+                break;
+            default:
+                nombreEstado = "";
+        }
+        return nombreEstado;
+    }
+}
