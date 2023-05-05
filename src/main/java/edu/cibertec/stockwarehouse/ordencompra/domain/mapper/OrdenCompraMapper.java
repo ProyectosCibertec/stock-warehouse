b976afd1458af6f58834fd9149c0ec94155b6866
ncmendoza <nelzoncriollo@gmail.com>
2023-05-05 00:07:31 -0500

    se agrega el mantenimiento de orden de compra

---
+++ src/main/java/edu/cibertec/stockwarehouse/ordencompra/domain/mapper/OrdenCompraMapper.java
@@ -0,0 +1,26 @@
+package edu.cibertec.stockwarehouse.ordencompra.domain.mapper;
+
+import edu.cibertec.stockwarehouse.ordencompra.domain.dto.OrdenCompraCreateDTO;
+import edu.cibertec.stockwarehouse.ordencompra.domain.dto.OrdenCompraDTO;
+import edu.cibertec.stockwarehouse.ordencompra.domain.dto.OrdenCompraUpdateDTO;
+import edu.cibertec.stockwarehouse.ordencompra.domain.model.OrdenCompra;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+
+@Mapper
+public interface OrdenCompraMapper {
+
+    OrdenCompraMapper instancia = Mappers.getMapper(OrdenCompraMapper.class);
+
+    OrdenCompraDTO ordenCompraAOrdenCompraDTO(OrdenCompra ordenCompra);
+
+    OrdenCompra ordenCompraDTOAOrdenCompra(OrdenCompraDTO ordenCompraDTO);
+
+    OrdenCompra ordenCompraCreateDTOAOrdenCompra(OrdenCompraCreateDTO oredenOrdenCompraCreateDTO);
+
+    OrdenCompra ordenCompraUpdateDTOAOrdenCompra(OrdenCompraUpdateDTO ordenCompraUpdateDTO);
+
+    List<OrdenCompraDTO> listaOrdenesCompraAOrdenCompraDTO(List<OrdenCompra> listaOrdenCompras);
+}
