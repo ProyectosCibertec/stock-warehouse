453aa77adf69cc5ddeaf3951453f1739cc79b0ce
JorgeChavezG <jorge10009@hotmail.com>
2023-05-21 22:08:44 -0500

    Jorge
    Ultimo avance jorge

--- backend/src/main/java/edu/cibertec/stockwarehouse/detalleinventario/domain/dto/DetalleInventarioUpdateDTO.java
+++ backend/src/main/java/edu/cibertec/stockwarehouse/detalleinventario/domain/dto/DetalleInventarioUpdateDTO.java
@@ -1,15 +1,19 @@
 package edu.cibertec.stockwarehouse.detalleinventario.domain.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class DetalleInventarioUpdateDTO {
 
     private int id_detalleInventario;
     private int id_cab_inventario;
-    private String nro_orden_compra;
+    //private String nro_orden_compra;
     private int id_producto;
     private int cantidad;
     private String observaciones;
