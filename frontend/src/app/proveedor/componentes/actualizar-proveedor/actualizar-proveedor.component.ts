979f7319ff13acfdb8f530c431ded5152837e47b
ASOTON2705 <i202020496@cibertec.edu.pe>
2023-05-23 16:00:37 -0500

    Actualización front

---
+++ frontend/src/app/proveedor/componentes/actualizar-proveedor/actualizar-proveedor.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-actualizar-proveedor',
+  templateUrl: './actualizar-proveedor.component.html',
+  styleUrls: ['./actualizar-proveedor.component.css']
+})
+export class ActualizarProveedorComponent {
+
+}
