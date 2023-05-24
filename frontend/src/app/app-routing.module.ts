0b529ac17d2d49a4fd8227ab636eadc62155932f
ASOTON2705 <i202020496@cibertec.edu.pe>
2023-05-23 19:05:04 -0500

    Se elabore la estructura del front y se logró conexión exitosa con el back primera prueba version beta

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,9 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListarProveedorComponent } from './proveedor/componentes/listar-proveedor/listar-proveedor.component';
 
-
 const routes: Routes = [
-  {path:'proveedor', component:ListarProveedorComponent}
+  {path:'proveedores', component:ListarProveedorComponent}
 ];
 
 @NgModule({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,22 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListarProveedorComponent } from './proveedor/componentes/listar-proveedor/listar-proveedor.component';
-import { RegistrarProveedorComponent } from './proveedor/componentes/registrar-proveedor/registrar-proveedor.component';
-import { ActualizarProveedorComponent } from './proveedor/componentes/actualizar-proveedor/actualizar-proveedor.component';
-import { EliminarProveedorComponent } from './proveedor/componentes/eliminar-proveedor/eliminar-proveedor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListarProveedorComponent,
-    RegistrarProveedorComponent,
-    ActualizarProveedorComponent,
-    EliminarProveedorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
